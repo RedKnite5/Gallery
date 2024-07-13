84f63319e11d2f078531c0fb196f1172f027ac32
Max <mr.awesome10000@gmail.com>
2024-07-13 14:55:11 -0700

    add setup file

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cur_file="$(realpath $0)"
+location="$(dirname $cur_file)"
+cd "$location"
+
+cnf_file="$(find . -name *.cnf)"
+filename="${cnf_file::-4}"
+cp "$cnf_file" "/etc/nginx/sites-available/$filename"
+ln -s "/etc/nginx/sites-available/$filename" "/etc/nginx/sites-enabled/"
+
+service_file="$(find . -name *.service)"
+service_filename="${service_file::-8}"
+
+cp "$service_file" "/etc/systemd/system/gallery.service"
+systemctl daemon-reload
+systemctl stop gallery
+systemctl start gallery
+nginx -s reload

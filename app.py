6e45d2558a5ed0861f789b565a128aba8f57b6f7
Max <mr.awesome10000@gmail.com>
2024-07-15 08:26:41 -0700

    .gitignore

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import os
 import json
 from pprint import pprint
 from pathlib import Path
+import logging
 
 from flask import Flask, jsonify, request
+
 
 # service file from
 # https://medium.com/swlh/mini-project-deploying-python-application-with-nginx-30f9b25b195
@@ -18,6 +20,9 @@ from flask import Flask, jsonify, request
 # sudo nginx -s reload
 
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="app.log", level=logging.DEBUG)
+
 conn = sqlite3.connect("secrets_and_data/database.db")
 
 
@@ -25,9 +30,16 @@ app = Flask(__name__)
 
 @app.route("/list-media/")
 def list_images():
+	logger.debug("/list-media/")
 	res = conn.execute("SELECT filename, description FROM media ORDER BY time DESC")
 	data = res.fetchall()
 	return jsonify(data)
+
+@app.route("/saving/")
+def save_tags():
+	logger.debug("/saving/")
+	
+
 
 if __name__ == "__main__":
 	app.run(debug=True, host="0.0.0.0")

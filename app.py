61431075e528495afccb22e951ac370f2cf1cb40
Max <mr.awesome10000@gmail.com>
2024-07-15 22:54:37 -0700

    can now update tags

--- app.py
+++ app.py
@@ -24,21 +24,44 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(filename="app.log", level=logging.DEBUG)
 
 conn = sqlite3.connect("secrets_and_data/database.db")
-
+cur = conn.cursor()
 
 app = Flask(__name__)
 
 @app.route("/list-media/")
 def list_images():
 	logger.debug("/list-media/")
-	res = conn.execute("SELECT filename, description FROM media ORDER BY time DESC")
+	res = cur.execute("SELECT filename, description FROM media ORDER BY time DESC")
 	data = res.fetchall()
 	return jsonify(data)
 
-@app.route("/saving/")
+@app.route("/saving/", methods=["POST"])
 def save_tags():
+	logger.debug(request.method)
+	logger.debug(getattr(request, "json", "default"))
 	logger.debug("/saving/")
-	
+
+	tags = request.json["tags"]
+	name = request.json["name"]
+	file = name.split("/")[-1]
+
+	res = cur.execute("SELECT description FROM media WHERE filename = ?", (file,))
+	data = res.fetchall()
+	cur.execute("UPDATE media SET description = ? WHERE filename = ?", (tags, file))
+
+	conn.commit()
+
+	logger.debug("data: %s", data)
+	logger.info(f"updating {file} from {data[0][0]!r} to {tags!r}")
+
+
+	res = cur.execute("SELECT description FROM media WHERE filename = ?", (file,))
+	data = res.fetchall()
+	logger.debug("data 2: %s", data)
+
+
+	return "200"
+
 
 
 if __name__ == "__main__":
